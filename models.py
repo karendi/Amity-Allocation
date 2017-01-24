4677ad182bef6525b0b11fbd68f72dd7f6ee3a6e
karendi <sharonkarendi16@gmail.com>
2017-01-24 08:20:07 +0300

    changed the database models

--- models.py
+++ models.py
@@ -1,17 +1,19 @@
 import os
 import sys
-from sqlalchemy import Column , ForeignKey , Integer , String , BigInteger , Text
+from sqlalchemy import Column, ForeignKey, Integer, String, BigInteger, Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
+
+
 class Amity_Population(Base):
     """ Table with all the people found in Amity """
 
     __tablename__ = 'Amity_Population'
 
-    Employee_id = Column(Integer , primary_key= True)
+    Employee_id = Column(Integer, primary_key=True)
     Employee_Type = Column(String(25))
 
 
@@ -20,22 +22,30 @@ class Amity_Allocation(Base):
 
     __tablename__ = 'Amity_Allocation'
 
-    id = Column(BigInteger , primary_key = True)
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
     Room_name = Column(String(25))
     Allocated_People = Column(Text)
+
+    def __init__(self, Room_name=None, Allocated_People=None, id=None):
+        self.id = id
+        self.Room_name = Room_name
+        self.Allocated_People = Allocated_People
+
 
 class Amity_Offices(Base):
     __tablename__ = 'Amity_Offices'
 
-    Room_name = Column(String(25) , primary_key = True)
+    Room_name = Column(String(25), primary_key=True)
+
 
 class Amity_Living_space(Base):
     __tablename__ = 'Amity_Living_space'
 
-    Room_name = Column(String(25) , primary_key = True)
+    Room_name = Column(String(25), primary_key=True)
 
-#create an engine that stores data in the local directory
+
+# create an engine that stores data in the local directory
 engine = create_engine('sqlite:///amity_allocation.db')
 
-#create all tables is the engine.It is the same as Create Table in sql
+# create all tables is the engine.It is the same as Create Table in sql
 Base.metadata.create_all(engine)
